CyanogenMod/android_system_vold

vold: Native NTFS kernel driver support
Change-Id: I825b2840a03e5cdab9b68f13fd5170acbbcec2e5

Android.mk
@@ -115,6 +115,10 @@ ifeq ($(TARGET_KERNEL_HAVE_EXFAT),true)
 LOCAL_CFLAGS += -DCONFIG_KERNEL_HAVE_EXFAT
 endif
 
+ifeq ($(TARGET_KERNEL_HAVE_NTFS),true)
+LOCAL_CFLAGS += -DCONFIG_KERNEL_HAVE_NTFS
+endif
+
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
 LOCAL_STATIC_LIBRARIES := libvold $(common_static_libraries)
 

Turbo-ROM/system_vold

Android.mk
@@ -81,6 +81,10 @@ ifeq ($(TARGET_KERNEL_HAVE_EXFAT),true)
 vold_cflags += -DCONFIG_KERNEL_HAVE_EXFAT
 endif
 
+ifeq ($(TARGET_KERNEL_HAVE_NTFS),true)
+vold_cflags += -DCONFIG_KERNEL_HAVE_NTFS
+endif
+
 include $(CLEAR_VARS)
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk